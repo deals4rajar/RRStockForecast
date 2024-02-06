2e27d4b4e6f935cc87dfb1f0ceb3116956649893
deals4rajar <158779653+deals4rajar@users.noreply.github.com>
2024-02-06 12:47:29 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 # pip install streamlit fbprophet yfinance plotly
 import streamlit as st
-from neuralprophet import NeuralProphet
 import yfinance as yf
 import pandas as pd
 import matplotlib.pyplot as plt
