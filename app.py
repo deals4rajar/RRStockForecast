a9d1cd3a08dd9f1bb724d4db915b4f9600180225
deals4rajar <158779653+deals4rajar@users.noreply.github.com>
2024-02-09 10:00:27 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 import yfinance as yf
-from keras.models import load_model
+#from keras.models import load_model
 from sklearn.preprocessing import MinMaxScaler
 import streamlit as st
 import joblib
